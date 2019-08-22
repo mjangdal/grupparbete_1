5fbb57a0de76bfff5490ea5c3afead794f8524a7
Mjangdal <martin.jangdal@ffcg.se>
2019-08-22 13:27:04 +0200

    martins animationer

--- martin.js
+++ martin.js
@@ -1,46 +1,65 @@
+
+
+
 function checkForVisibility() {
   var martins = document.querySelectorAll(".martin");
-  let slides = document.querySelectorAll(".martin-box")
+  let slide = document.querySelectorAll(".martin-box");
+  //sliding in the headlines
   martins.forEach(function(martin) {
     if (isElementInViewport(martin)) {
-
       martin.classList.add("martin-visible")
-    }else martin.classList.remove("martin-visible");
-    
-    
-    
-    
-  });
-
-  slides.forEach(function(slide){
-    if (slide.dataset.anime === true) return;
-    else{
-      slide.dataset.anime = true;
-      anime({
-        targets: '.martin-box',
-        translateY: [250,0],
-        translateX:[250,0],
-        opacity: [0,1],
-        rotate: [0,360],
-        delay: function(el, i, l) {
-          return i * 100;
-        },
-      })
+    }else {
+      martin.classList.remove("martin-visible");
     }
   });
+  //spinning boxex entrance
+  slide.forEach(function(slide){
+    
+    if (isElementInViewport(slide)){
+      //Checking if the boxes have beed shown
+      if (slide.dataset.anime == 'true'){
+        return;
+      }
+      else{ //if not show spin them in
+        anime({
+          targets: '.martin-box',
+          translateY: [(anime.random(50,500)),0],
+          translateX:[(anime.random(50,250)),0],
+          opacity: [0,1],
+          rotate: [0,360],
+          delay: function(el, i, l) {
+            return i * 100;
+          },
+        })
+        slide.dataset.anime = 'true';
+      }
+    }
+    else slide.dataset.anime = 'false' ;
+  });
 }
+
 
 function isElementInViewport (el) {
   var rect = el.getBoundingClientRect();
 
   return (
-    rect.top >= -50 &&
+    rect.top >= -0 &&
     rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) + 200 
   );
 }
 
 if (window.addEventListener) {
-  addEventListener('DOMContentLoaded', checkForVisibility, false); 
-  addEventListener('load', checkForVisibility, false);
   addEventListener('scroll', checkForVisibility,false)
 }
+//Making the boxes dissapear
+document.querySelectorAll('.martin-box').forEach(box => {
+  box.addEventListener("click", function(){
+    anime({
+    targets: this,
+    opacity: [1,0],
+    scale:2,
+    })
+  });
+});
+
+
